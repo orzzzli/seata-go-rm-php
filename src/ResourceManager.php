589bdf02cafedca04138c9d30c4c4b0a00cb4e04
ligongxiang <ligongxiang@rouchi.com>
2020-04-24 17:36:22 +0800

    add base struct

---
+++ src/ResourceManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @user: ligongxiang (ligongxiang@rouchi.com)
+ * @date : 2020/4/24
+ * @version : 1.0
+ * @file : ResourceManager.php
+ * @desc :
+ */
+
+namespace ResourceManager;
+
+class ResourceManager
+{
+
+}
